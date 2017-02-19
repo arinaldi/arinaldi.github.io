47b83c5069e17f680c80c2bc770abf9fbe611a22
Tony <arinaldi@vt.edu>
2017-02-19 15:35:25 -0600

    Added lazy loading for images

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    responsive:{
-        0:{
-            dots: false,
-            items: 1,
-            slideBy: 1
-        },
-        768:{
-            items: 2,
-            slideBy: 2
-        },
-        992:{
-            items: 4,
-            slideBy: 4
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        lazyLoad: true,
+        navText: ['Prev', 'Next'],
+        responsive: {
+            0:{
+                dots: false,
+                items: 1,
+                slideBy: 1,
+                autoHeight: true
+            },
+            768:{
+                items: 2,
+                slideBy: 2
+            },
+            992:{
+                items: 4,
+                slideBy: 4
+            }
         }
-    }
+    });
 });
-
-$('.owl-prev').text('Prev');
-$('.owl-next').text('Next');
